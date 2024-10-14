c5174ae5b748f8d1658138cb9fac634682dabdb6
zahqa <zahqa@ZAHRA>
2024-10-14 18:14:54 +0200

    Add project files.

---
+++ Lektion-TheForest-Arv/Animal.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion_TheForest_Arv
+{
+    internal class Animal //Basklass
+    {
+        //I klassen Animal kan vi ha flera klasser i samma namespace
+        public int Weight { get; set; }
+        public bool Nocturnal { get; set; }
+
+        public Animal(int weight, bool nocturnal) 
+        {
+            Weight = weight;
+            Nocturnal = nocturnal;
+
+
+        }
+        public virtual void Move(bool day)
+        {
+            //Inget händer
+        }
+
+    }
+
+    //Vi har en Uggla men den ärver egenskaper från Animal
+    class Owl : Animal
+    {
+        public int Wingspan { get; set; }
+
+        public Owl(int weight, bool nocturnal, int wingspan) 
+            : base (weight, nocturnal)
+        {
+            Wingspan = wingspan;
+        }
+        public override void Move(bool day)
+        {
+            if (Nocturnal == true && day == false)
+            {
+                Console.WriteLine("Ugglan flyger över träden.\n");
+            }
+            else
+            {
+                Console.WriteLine("Ugglan sover på ett träd\n");
+            }
+        }
+
+
+    }
+
+    class Horse : Animal // Subklass
+    {
+        public int HayPerDay { get; set; }
+        public Horse(int weight, bool nocturnal, int hayPerDay) :base (weight, nocturnal) 
+        {
+            HayPerDay = hayPerDay;  
+        }
+
+        public override void Move(bool day)
+        {
+            if (Nocturnal == false && day == true)
+            {
+                Console.WriteLine("Hästen galopperar.\n");
+            }
+            else
+            {
+                Console.WriteLine("Hästen sover under ett träd. \n");
+            }
+        }
+    }
+
+    class Dolphin : Animal
+    {
+        public int DistancePerDay { get; set; }
+        public Dolphin(int weight, bool nocturnal, int distancePerDay) : base (weight, nocturnal) 
+        {
+            DistancePerDay = distancePerDay;
+        }
+        public override void Move(bool day)
+        {
+            if (Nocturnal == false && day == true)
+            {
+                Console.WriteLine("Dolfinen simmar.\n");
+            }
+            else
+            {
+                Console.WriteLine("Dolfinen sover i havet\n");
+            }
+        }
+    }
+}
---
+++ Lektion-TheForest-Arv/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Lektion_TheForest_Arv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Forest \nTryck på d för dag n för natt!");
+            List<Animal> forrest = new List<Animal>();
+
+            //Vi har en lista med djur i skogen
+            forrest.Add(new Owl(3500, true, 94));
+            forrest.Add(new Horse(1200, false, 30));
+            forrest.Add(new Dolphin(100000, false, 3750));
+            forrest.Add(new Owl(4500, true, 89));
+            forrest.Add(new Dolphin(4500, false, 6000));
+            forrest.Add(new Horse(5000, false, 400));
+
+            //Vi kan skapa en ny animal men vi vet inte vilken typ av animal detta är.
+            //forrest.Add(new Animal(12300, false));
+
+            //Men på sättet nedan kan vi lättare skapa ny animal.
+            //List<Owl> OwlsInForest = new List<Owl>();
+            //OwlsInForest.Add(new Owl(3500, true, 94));
+
+            //Traditionella sättet att skapa en objekt
+            //Owl owl1 = new Owl(3500, true, 94);
+
+
+
+            bool day = true;
+            while (true)
+            {
+                string key = Console.ReadKey().Key.ToString();
+
+                Console.Clear();
+                
+
+                // Console.WriteLine(key);
+                switch (key)
+                {
+                    case "D":
+                        day = true;
+                        Console.WriteLine("Nu är det dag!");
+                        break;
+                    case "N":
+                        day = false;
+                        Console.WriteLine("Nu är det natt!");
+                        break;
+
+                }
+
+                foreach (Animal animal in forrest)
+                {
+                    Console.Write(animal.GetType().Name + (" Vikt " + animal.Weight + " nattdjur: " + animal.Nocturnal));
+                    if (animal is Owl)
+                    {
+                        Console.WriteLine(" Wingspan: " + ((Owl)animal).Wingspan);
+                    }
+                    if (animal is Horse)
+                    {
+                        Console.WriteLine(" Hay per day: " + ((Horse)animal).HayPerDay);
+                    }
+                    if (animal is Dolphin) // Detta är samma som --> if (animal is Dolphin) = (Dolphin)animlal;
+                                           //Console.WriteLine(" Distance per day: " + dolphin.DistancePerDay);
+                    {
+                        Console.WriteLine(" Distans per day: " + ((Dolphin)animal).DistancePerDay);
+                    }
+
+                    animal.Move(day);
+                }
+                
+
+            }
+            
+        }
+    }
+}
